f73280a17c13abd95d932bb4fb4befb7ccc0b562
Eric Fritz <eric@eric-fritz.com>
2017-03-15 20:41:18 -0500

    (Closes #2) Add BlockUntilSuccessTimeout.

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package watchdog
 
-import "github.com/efritz/backoff"
+import (
+	"fmt"
+	"time"
 
-// BlockUntilSuccess creates a watcher that fires until the given retry
-// function returns a success, then disables the watcher. This function
-// is synchronous.
+	"github.com/efritz/backoff"
+)
+
+// BlockUntilSuccess creates a transient watcher that fires the given
+// retry function until success.
 func BlockUntilSuccess(retry Retry, backoff backoff.Backoff) {
 	watcher := NewWatcher(retry, backoff)
+	defer watcher.Stop()
+
 	<-watcher.Start()
-	watcher.Stop()
+}
+
+// BlockUntilSuccessTimeout creates a transient watcher that fires the
+// given retry function until success or until the specified timeout
+// elapses. An error is returned in the later case.
+func BlockUntilSuccessTimeout(retry Retry, backoff backoff.Backoff, timeout time.Duration) error {
+	watcher := NewWatcher(retry, backoff)
+	defer watcher.Stop()
+
+	select {
+	case <-watcher.Start():
+		return nil
+	case <-time.After(timeout):
+		return fmt.Errorf("timeout elapsed")
+	}
 }
